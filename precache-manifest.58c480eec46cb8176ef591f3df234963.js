edf540b9cf0b4d30531e601236ff3660b4378384
anibalajt <anibalajt@gmail.com>
2020-09-08 00:44:21 +0800

    Updates

---
+++ precache-manifest.58c480eec46cb8176ef591f3df234963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcef122e962711d243927a9287be612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ada41101b2ed54597e",
+    "url": "/static/css/main.f9fa975a.chunk.css"
+  },
+  {
+    "revision": "83cf0be6dfda42b628d2",
+    "url": "/static/js/2.345bc66c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.345bc66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ada41101b2ed54597e",
+    "url": "/static/js/main.b4c8c353.chunk.js"
+  },
+  {
+    "revision": "1676fefdccce6c103122",
+    "url": "/static/js/runtime-main.710e0d7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
